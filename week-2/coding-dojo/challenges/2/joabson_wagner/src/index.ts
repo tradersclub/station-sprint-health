6dc4ce9cf1d200f391dd97cb9dd3e140b7b41930
wagneresser <waesser@gmail.com>
2022-05-12 17:03:26 -0300

    part 1

--- week-2/coding-dojo/challenges/2/joabson_wagner/src/index.ts
+++ week-2/coding-dojo/challenges/2/joabson_wagner/src/index.ts
@@ -1,3 +1,40 @@
-import {sum} from "./utils";
+type User = {
+    id: number;
+    name: string;
+}
 
-console.log(sum(1,2))
+// função para gerar lista 1.000.000
+
+
+// função para gerar 1 user
+const createUser = (_index: number, index: number) => {
+    const user: User = {
+        id: index,
+        name: 'user_' + index
+    }
+
+    return user
+}
+
+// função de busca
+// const searchUser = (id: number): User | undefined => {
+//     return list.find(user => user.id === id)
+// }
+
+// // função de delete
+// const deleteUser = (id: number): User[] => {
+//     return list.filter(user => user.id !== id)
+// }
+
+const generateList = (length: number) => {
+    const list = Array.from({ length }, createUser)
+    return list
+}
+
+
+const main = () => {
+    const listUsers = generateList(10)
+    console.log(`🚀 ~ listUsers`, listUsers)
+}
+
+main()
