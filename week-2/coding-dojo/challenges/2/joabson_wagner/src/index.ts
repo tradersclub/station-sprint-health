1c0ae04625881fd542928d73259b3e88dde2efa2
leomiranda-tc <leonardo.miranda@tradersclub.com.br>
2022-05-12 17:32:56 -0300

    joabson

--- week-2/coding-dojo/challenges/2/joabson_wagner/src/index.ts
+++ week-2/coding-dojo/challenges/2/joabson_wagner/src/index.ts
@@ -2,39 +2,40 @@ type User = {
     id: number;
     name: string;
 }
-
-// função para gerar lista 1.000.000
-
-
-// função para gerar 1 user
-const createUser = (_index: number, index: number) => {
+const lengthList = 1000000;
+const createUser = (_: number, index: number) => {
     const user: User = {
         id: index,
         name: 'user_' + index
     }
-
     return user
 }
-
-// função de busca
-// const searchUser = (id: number): User | undefined => {
-//     return list.find(user => user.id === id)
-// }
-
-// // função de delete
-// const deleteUser = (id: number): User[] => {
-//     return list.filter(user => user.id !== id)
-// }
-
-const generateList = (length: number) => {
+const searchUser = (list: User[], id: number): User | undefined => {
+    return list.find(user => user.id === id)
+}
+const deleteUser = (list: User[], id: number): User[] => {
+    return list.filter(user => user.id !== id)
+}
+const generateList = (length: number): User[] => {
     const list = Array.from({ length }, createUser)
     return list
 }
 
 
 const main = () => {
-    const listUsers = generateList(10)
-    console.log(`🚀 ~ listUsers`, listUsers)
-}
+    const listUsers = generateList(lengthList)
+    console.log(`:foguete: ~ listUsers`, listUsers.length)
+    console.time('search')
 
+    searchUser(listUsers, lengthList)
+    console.timeEnd('search')
+    console.time('deleteUser')
+    const newList = deleteUser(listUsers, 1)
+    // console.log(`:foguete: ~ delete`, newList.length)
+    console.timeEnd('deleteUser')
+    console.time('createUser')
+    const userTest = createUser(10, listUsers.length + 1);
+    // console.log(`:foguete: ~ userTest`, userTest)
+    console.timeEnd('createUser')
+}
 main()
