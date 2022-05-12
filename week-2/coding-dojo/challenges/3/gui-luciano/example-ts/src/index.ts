ccb480215049928ad1419f7ebd62b6fd44ccdae4
Guilherme Ferreira <guilherme.ferreira@tc.com.br>
2022-05-12 14:46:40 -0300

    first commit: commit gui

---
+++ week-2/coding-dojo/challenges/3/gui-luciano/example-ts/src/index.ts
@@ -0,0 +1,14 @@
+const tabuleiro = [
+  ["x", "x", "x"],
+  ["o", "x", "o"],
+  ["x", "o", "x"],
+];
+//"x", "x", "x", "b1", "b2", "b3", "c1", "c2", "c3"
+
+function validHorizontal() {
+  const row = tabuleiro[0];
+
+  tabuleiro[0].map((celula) => {
+    celula === "";
+  });
+}
