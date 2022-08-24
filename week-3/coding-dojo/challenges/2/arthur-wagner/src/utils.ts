5e79a3610e788381fbde6f98f4fec0ab160160c5
Arthur Geron <arthur.geron@tc.com.br>
2022-08-24 18:14:06 -0300

    feat: use for and exit immediately on fail

--- week-3/coding-dojo/challenges/2/arthur-wagner/src/utils.ts
+++ week-3/coding-dojo/challenges/2/arthur-wagner/src/utils.ts
@@ -5,41 +5,42 @@ interface Dictionary {
   positionsIterated: number;
 }
 
-export const isDictionary = (firstWord: string, secondWOrd:string): Dictionary => {
-  if (firstWord.length !== secondWOrd.length) {
-    return {
-      isDictionary: false,
-      positionsIterated: 0,
-      dictionary: {},
-      mappedLetters: {}
-    };
-  }
-  
-  const parsedA: Array<string> = firstWord.split('');
-
-  return parsedA.reduce<Dictionary>((acc: Dictionary, current: string, index: number) => {
-    const dictionaryValue = acc.dictionary[current];
-    acc.positionsIterated = index + 1;
-
-    if (!dictionaryValue) {
-      if (acc.mappedLetters[secondWOrd[index]]) {
-        acc.isDictionary = false
-        return acc
-      }
-      acc.dictionary[current] = secondWOrd[index];
-      acc.mappedLetters[secondWOrd[index]] = true
-      return acc;
-    }
-    if (dictionaryValue !== secondWOrd[index]) {
-      acc.isDictionary = false;
-      return acc;
-    }
-
-    return acc;
-  }, {
+export const isDictionary = (firstWord: string, secondWord:string): Dictionary => {
+  const acc: Dictionary = {
     isDictionary: true,
     positionsIterated: 0,
     dictionary: {},
-    mappedLetters: {},
-  });
+    mappedLetters: {}
+  };
+
+  const firstWordLength = firstWord.length;
+  const secondWordLength = secondWord.length;
+  
+  if (firstWordLength !== secondWordLength) {
+    acc.isDictionary = false;
+    return  acc;
+  }
+
+  for (let i = 0; i < firstWordLength; i++) {
+    const current = firstWord[i];
+    const dictionaryValue = acc.dictionary[current];
+
+    acc.positionsIterated = i + 1;
+
+    if (!dictionaryValue) {
+      if (acc.mappedLetters[secondWord[i]]) {
+        acc.isDictionary = false
+        return acc
+      }
+      acc.dictionary[current] = secondWord[i];
+      acc.mappedLetters[secondWord[i]] = true
+      continue;
+    }
+    if (dictionaryValue !== secondWord[i]) {
+      acc.isDictionary = false;
+      return acc;
+    }
+  }
+  
+  return acc;
 }
