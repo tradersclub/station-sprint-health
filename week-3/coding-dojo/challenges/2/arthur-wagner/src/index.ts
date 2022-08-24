d9b3657f0c643ca5c8278681b5225d88f4f73dd4
leomiranda-tc <leonardo.miranda@tc.com.br>
2022-08-24 17:49:44 -0300

    fix: adjustments

--- week-3/coding-dojo/challenges/2/arthur-wagner/src/index.ts
+++ week-3/coding-dojo/challenges/2/arthur-wagner/src/index.ts
@@ -1,27 +1,8 @@
 import {isDictionary} from "./utils";
+import {isIsomorphic, isIsomorphic2} from "./utils_leo";
 import Benchmark from 'benchmark'
 
 var suite = new Benchmark.Suite;
-
-function isAlphabetSubstituted(word1: string, word2: string) {
-  let alphabet = new Map()
-  
-  for (let index in word1 as unknown as string[]) {
-      const letterWanted = alphabet.get(word2[index])
-
-      if (letterWanted && letterWanted != word1[index]) {
-          return false
-      }
-
-      alphabet.set(word2[index], word1[index])
-  }
-
-  return true
-}
-
-function isIsomorphic(word1: string, word2: string) {
-  return isAlphabetSubstituted(word1, word2) && isAlphabetSubstituted(word2, word1)
-};
 
 suite.add('test isDictionary', function() {
   isDictionary('paper', 'title')
@@ -29,10 +10,13 @@ suite.add('test isDictionary', function() {
 .add('test isIsomorphic', function() {
   isIsomorphic('paper', 'title')
 })
+.add('test isIsomorphic2', function() {
+  isIsomorphic2('paper', 'title')
+})
 .on('cycle', function(event: Benchmark.Event) {
-console.log(String(event.target));
+  console.log(String(event.target));
 })
 .on('complete', function(this: Benchmark.Suite) {
-console.log('Fastest is ' + this.filter('fastest').map('name'));
+  console.log('Fastest is ' + this.filter('fastest').map('name'));
 })
 .run({ 'async': true });
---
+++ week-3/coding-dojo/challenges/2/arthur-wagner/src/utils_leo.ts
@@ -0,0 +1,41 @@
+function isAlphabetSubstituted(word1: string, word2: string) {
+    let alphabet = new Map()
+  
+    for (let index = 0; index < word1.length; index++) {
+        const letterWanted = alphabet.get(word2[index])
+  
+        if (letterWanted && letterWanted != word1[index]) {
+            return false
+        }
+  
+        alphabet.set(word2[index], word1[index])
+    }
+  
+    return true
+}
+  
+export function isIsomorphic(word1: string, word2: string) {
+    return isAlphabetSubstituted(word1, word2) && isAlphabetSubstituted(word2, word1)
+};
+  
+export function isIsomorphic2(word1: string, word2: string) {
+    const alphabet = new Map()
+    const mappedLetters = new Map()
+  
+    for (let index = 0; index < word1.length; index++) {
+        const letterWanted = alphabet.get(word2[index])
+  
+        if (letterWanted && letterWanted != word1[index]) {
+            return false
+        }
+  
+        if (!letterWanted && mappedLetters.get(word1[index]) === true) {
+          return false
+        }
+  
+        mappedLetters.set(word1[index], true)
+        alphabet.set(word2[index], word1[index])
+    }
+  
+    return true
+  };
