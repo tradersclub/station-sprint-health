4edbe6d7c60563e0acfd725dcee161d27e8d0e30
Wagner Esser <waesser@gmail.com>
2022-08-24 17:10:16 -0300

    feat(week-3): challenge 2 - isDictionary resolution

--- week-3/coding-dojo/challenges/2/arthur-wagner/src/__test__/index.test.ts
+++ week-3/coding-dojo/challenges/2/arthur-wagner/src/__test__/index.test.ts
@@ -1,7 +1,27 @@
 import {isDictionary} from "../utils";
 
 describe("run test", () => {
-  test('adadsasd', () => {
+  test('should EGG and ADD is dictionary', () => {
     expect(isDictionary('egg', 'add').isDictionary).toBeTruthy();
+    expect(isDictionary('add', 'egg').isDictionary).toBeTruthy();
+  });
+
+  test('should FOO and BAR is dictionary', () => {
+    expect(isDictionary('foo', 'bar').isDictionary).toBeFalsy();
+    expect(isDictionary('bar', 'foo').isDictionary).toBeFalsy();
+  });
+
+  test('should PAPER and TITLE is dictionary', () => {
+    expect(isDictionary('paper', 'title').isDictionary).toBeTruthy();
+    expect(isDictionary('title', 'paper').isDictionary).toBeTruthy();
+  });
+
+  test('should PPAPER and TTITLE is dictionary', () => {
+    expect(isDictionary('ppaper', 'ttitle').isDictionary).toBeTruthy();
+    expect(isDictionary('ttitle', 'ppaper').isDictionary).toBeTruthy();
+  });
+
+  test('should ARTHUR and LEO is dictionary', () => {
+    expect(isDictionary('arthur', 'leo').isDictionary).toBeFalsy();
   });
 })
--- week-3/coding-dojo/challenges/2/arthur-wagner/src/index.ts
+++ week-3/coding-dojo/challenges/2/arthur-wagner/src/index.ts
@@ -1,3 +1,38 @@
-import {sum} from "./utils";
+import {isDictionary} from "./utils";
+import Benchmark from 'benchmark'
 
-console.log(sum(1,2))
+var suite = new Benchmark.Suite;
+
+function isAlphabetSubstituted(word1: string, word2: string) {
+  let alphabet = new Map()
+  
+  for (let index in word1 as unknown as string[]) {
+      const letterWanted = alphabet.get(word2[index])
+
+      if (letterWanted && letterWanted != word1[index]) {
+          return false
+      }
+
+      alphabet.set(word2[index], word1[index])
+  }
+
+  return true
+}
+
+function isIsomorphic(word1: string, word2: string) {
+  return isAlphabetSubstituted(word1, word2) && isAlphabetSubstituted(word2, word1)
+};
+
+suite.add('test isDictionary', function() {
+  isDictionary('paper', 'title')
+})
+.add('test isIsomorphic', function() {
+  isIsomorphic('paper', 'title')
+})
+.on('cycle', function(event: Benchmark.Event) {
+console.log(String(event.target));
+})
+.on('complete', function(this: Benchmark.Suite) {
+console.log('Fastest is ' + this.filter('fastest').map('name'));
+})
+.run({ 'async': true });
