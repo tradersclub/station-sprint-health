8c00903422169748c610a1832edb58c0a190f8a8
leomiranda-tc <leonardo.miranda@tc.com.br>
2023-01-06 16:15:39 -0300

    challenge 2

---
+++ week-4/coding-dojo/challenges/2/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+var wordsWithAlphabet = make([]string, 0)
+
+func main() {
+	var chanNewWord = make(chan string, 100)
+	go poolWords(chanNewWord)
+
+	http.HandleFunc("/set", func(w http.ResponseWriter, r *http.Request) {
+		word := r.URL.Query().Get("word")
+		chanNewWord <- word
+	})
+
+	http.HandleFunc("/get", func(w http.ResponseWriter, r *http.Request) {
+		json.NewEncoder(w).Encode(wordsWithAlphabet)
+	})
+
+	http.ListenAndServe(":8080", nil)
+}
+
+func poolWords(data <-chan string) {
+	// implement here
+}
--- week-4/coding-dojo/challenges/2/readme.md
+++ week-4/coding-dojo/challenges/2/readme.md
@@ -1,2 +1,13 @@
 # Challenge 2
 
+Implement a function that receive a word from the channel called `chanNewWord`, check if this word doesn't have any repeated letter, and store this info in slice called `wordsWithAlphabet`.
+
+You can insert a new word just sending a request like this:
+
+``[GET] http://localhost:8080/set?word=myword``
+
+And you can check all the slice doing a request like this:
+
+``[GET] http://localhost:8080/get``
+
+Ps.: You can use just the browser to run this requests
