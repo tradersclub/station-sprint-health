5d171acf064e79a105fee6013c28c5e03d787f3c
leomiranda-tc <leonardo.miranda@tc.com.br>
2023-02-15 11:36:47 -0300

    ts files

--- example-ts/src/index.ts
+++ example-ts/src/index.ts
@@ -1,18 +1,4 @@
 import {sum} from "./utils";
-import Benchmark from 'benchmark'
 
-var suite = new Benchmark.Suite;
 
-suite
-// bench test
-.add('test isDictionary', function() {
-  sum(1, 2)
-})
-// print results
-.on('cycle', function(event: Benchmark.Event) {
-  console.log(String(event.target));
-})
-.on('complete', function(this: Benchmark.Suite) {
-  console.log('Fastest is ' + this.filter('fastest').map('name'));
-})
-.run({ 'async': true });
+console.log(sum(2,3))
